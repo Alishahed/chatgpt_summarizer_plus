7ad7a6ecb999b2d04cbd8f7c80cfce84e7be26aa
Ali Shahed <ali.shahed@outlook.com>
2023-03-13 16:55:04 -0700

    pdf to txt is added.

--- app_util.py
+++ app_util.py
@@ -1,7 +1,22 @@
 import os
 import streamlit as st
+from pdfminer.high_level import extract_text
+
+
+def delete_file(saved_file):
+    if os.path.exists(saved_file):
+        os.remove(saved_file)
+
+def pdf_to_text(uploadedfile):
+    output_txt = extract_text(os.path.join("tempDir",uploadedfile.name),"r")
+    return output_txt
 
 def save_uploadedfile(uploadedfile):
-     with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
-         f.write(uploadedfile.getbuffer())
-     return st.success("Saved File:{} to tempDir".format(uploadedfile.name))
+    with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
+        f.write(uploadedfile.getbuffer())
+    text_string = pdf_to_text(uploadedfile)
+    with open(os.path.join("tempDir","tempfile.txt"),"w") as f:
+        f.write(text_string)
+        f.close()
+    return st.success("Uploading and processing File:{}".format(uploadedfile.name))
+    
--- gpt_app.py
+++ gpt_app.py
@@ -4,11 +4,14 @@ from dotenv import load_dotenv
 from google.oauth2 import service_account
 from google.cloud import storage
 import streamlit as st
-from app_util import save_uploadedfile
+from app_util import save_uploadedfile,delete_file
+import uuid
+
+
 
 #print(os.environ)
 #load_dotenv()
-
+max_token = 3000
 openai.api_key = st.secrets['open_ai_key']['OPENAI_API_KEY']
 #openai.api_key_path = '/home/ali/Projects/chatgpt_teacher/chatgpt_api_test/.env'
 
@@ -23,31 +26,35 @@ with st.sidebar:
     st.subheader("OR")
     article_url = st.text_input('Enter the Article URL here')
 if pdf_file is not None:
+    txt_file_name = str(uuid.uuid4())+".txt"
     file_details = {"FileName":pdf_file.name,"FileType":pdf_file.type}
     save_uploadedfile(pdf_file)
     bucket_name = client.bucket("summarization_chatgpt")
-    object_name_in_gcs_bucket = bucket_name.blob(pdf_file.name)
-    object_name_in_gcs_bucket.upload_from_filename(f'tempDir/{pdf_file.name}')
-    auth_url = f'https://storage.cloud.google.com/summarization_chatgpt/{pdf_file.name}'
+    object_name_in_gcs_bucket = bucket_name.blob(txt_file_name)
+    object_name_in_gcs_bucket.upload_from_filename(f'tempDir/tempfile.txt')
+
+    #object_name_in_gcs_bucket = bucket_name.blob(pdf_file.name)
+    #object_name_in_gcs_bucket.upload_from_filename(f'tempDir/{pdf_file.name}')
+    auth_url = f'https://storage.cloud.google.com/summarization_chatgpt/{txt_file_name}'
+    input_string = object_name_in_gcs_bucket.download_as_text()
+    input_string_cut = input_string[:max_token]
+    delete_file(f'tempDir/tempfile.txt')
+    delete_file(f'tempDir/{pdf_file.name}')
 else:
     auth_url = article_url
+    #pass
 
 if auth_url:
     response_summary = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-            messages=[
+        messages=[
                 {"role": "user", "content": st.secrets['chatgpt_queries']['summarizer_query1'] +
-                                            auth_url+'\n'+
+                                            input_string_cut +'\n'+
                                             st.secrets['chatgpt_queries']['summarizer_query2'] +
                                             st.secrets['chatgpt_queries']['summarizer_query3'] +
                                             st.secrets['chatgpt_queries']['summarizer_query4']}
-    ]
-    #     messages=[
-    #             {"role": "user", "content": f"Can you summarize this document and its results in 3 paragraph:{auth_url}'\n"+
-    #                                 f"What are the main results of this paper? Put it maximum 5 bullet.\n"+
-    #                                 f"What keywords Do I need to understand for understanding this paper?\n"+
-    #                                 f"Can you provide a tutorial to learn about each of the above keywords for a beginner? Please provide the link to the tutorials too.\n"}
-    # ]
+    ],
+    max_tokens = max_token
     )
 
     st.write(response_summary.choices[0].message.content)
