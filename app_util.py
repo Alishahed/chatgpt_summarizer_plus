7b7c95aab76d682c9f3af85e83f4addf3de86381
Ali Shahed <ali.shahed@outlook.com>
2023-03-13 21:13:16 -0700

    fixed indentation

--- app_util.py
+++ app_util.py
@@ -23,14 +23,14 @@ def save_uploadedfile(uploadedfile):
     return st.success("Uploading and processing File:{}".format(uploadedfile.name))
     
 def tag_visible(element):
-        if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
-            return False
-        if isinstance(element, Comment):
-            return False
-        return True
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
 
 def text_from_html(body):
-        soup = BeautifulSoup(body, 'html.parser')
-        texts = soup.findAll(text=True)
-        visible_texts = filter(tag_visible, texts)  
-        return u" ".join(t.strip() for t in visible_texts)
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)  
+    return u" ".join(t.strip() for t in visible_texts)
